7e7fea5b3b1c483407c3c16e66b18a6a679dc0ef
Ujwal Subedi <hrirks@live.com>
2021-07-01 17:47:44 +0200

    bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {NgbPaginationModule, NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+})
+
+@NgModule({
+  imports: [NgbPaginationModule, NgbAlertModule],
 })
 export class AppComponent {
   title = 'Artificial Intelligence (A.I.) Conjointment';
